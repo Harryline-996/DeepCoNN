7e4082bf39307db3bfa40f95b14b514023219186
zhaojinglong <15866997163@163.com>
2020-12-11 22:57:19 +0800

    调整了一下文件结构

--- README.md
+++ README.md
@@ -5,22 +5,21 @@ Lei Zheng, Vahid Noroozi, and Philip S Yu. 2017. Joint deep modeling of users an
 
 # Environments
   + python 3.8
-  + pytorch 1.6
+  + pytorch 1.70
 
 # Dataset
   You need to prepare the following documents:  
-  1. dataset(`/data/music/Digital_Music_5.json.gz`)  
-   Download from http://jmcauley.ucsd.edu/data/amazon (Choose Digital Music)
-  2. word2vec(`/data/embedding/GoogleNews-vectors-negative300.bin`)  
-   Download from https://code.google.com/archive/p/word2vec/  
-   Or get it from BaiduYun: https://pan.baidu.com/s/1ddaATD0GAPb8z_K2dmnV7A 提取码：l7ko   
+  1. dataset(`data/Digital_Music_5.json.gz`)  
+   Download from http://jmcauley.ucsd.edu/data/amazon (Choose Digital Music)  
+   Preprocess origin dataset in json format to train.csv,valid.csv and test.csv.  
+   ```
+   python preprocess.py
+   ```
+
+  2. Word Embedding(`embedding/glove.6B.50d.txt`)  
+   Download from https://nlp.stanford.edu/projects/glove
 
 # Running
-Preprocess origin dataset in json format to train.csv,valid.csv and test.csv.  
-**Rewrite some necessary settings** in this file before running it. 
-```
-python preprocess.py
-```
 
 Train and evaluate the model:
 ```
--- main.py
+++ main.py
@@ -4,36 +4,21 @@ import time
 import torch
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from gensim.models import KeyedVectors
 
 from config import Config
-from model import DeepCoNNDataset, DeepCoNN
-
-
-def date(format='%Y-%m-%d %H:%M:%S'):
-    return time.strftime(format, time.localtime())
-
-
-def predict_mse(model, dataloader):
-    mse, sample_count = 0, 0
-    with torch.no_grad():
-        for batch in dataloader:
-            user_reviews, item_reviews, ratings = map(lambda x: x.to(config.device), batch)
-            predict = model(user_reviews, item_reviews)
-            mse += F.mse_loss(predict, ratings, reduction='sum').item()
-            sample_count += len(ratings)
-    return mse / sample_count  # dataloader上的均方误差
+from model import DeepCoNN
+from utils import load_embedding, DeepCoNNDataset, predict_mse, date
 
 
 def train(train_dataloader, valid_dataloader, model, config, model_path):
     print(f'{date()}## Start the training!')
-    train_mse = predict_mse(model, train_dataloader)
-    valid_mse = predict_mse(model, valid_dataloader)
+    train_mse = predict_mse(model, train_dataloader, config.device)
+    valid_mse = predict_mse(model, valid_dataloader, config.device)
     print(f'{date()}#### Initial train mse {train_mse:.6f}, validation mse {valid_mse:.6f}')
     start_time = time.perf_counter()
 
     opt = torch.optim.Adam(model.parameters(), config.learning_rate, weight_decay=config.l2_regularization)
-    torch.optim.lr_scheduler.ExponentialLR(opt, config.learning_rate_decay)
+    lr_sch = torch.optim.lr_scheduler.ExponentialLR(opt, config.learning_rate_decay)
 
     best_loss, best_epoch = 100, 0
     for epoch in range(config.train_epochs):
@@ -50,8 +35,9 @@ def train(train_dataloader, valid_dataloader, model, config, model_path):
             total_loss += loss.item()
             total_samples += len(predict)
 
+        lr_sch.step()
         model.eval()  # 停止训练状态
-        valid_mse = predict_mse(model, valid_dataloader)
+        valid_mse = predict_mse(model, valid_dataloader, config.device)
         train_loss = total_loss / total_samples
         print(f"{date()}#### Epoch {epoch:3d}; train mse {train_loss:.6f}; validation mse {valid_mse:.6f}")
 
@@ -63,35 +49,30 @@ def train(train_dataloader, valid_dataloader, model, config, model_path):
     print(f'{date()}## End of training! Time used {end_time - start_time:.0f} seconds.')
 
 
-def test(dataloader, model_path):
+def test(dataloader, model):
     print(f'{date()}## Start the testing!')
     start_time = time.perf_counter()
-
-    best_model = torch.load(model_path)
-    test_loss = predict_mse(best_model, dataloader)
-
+    test_loss = predict_mse(model, dataloader, config.device)
     end_time = time.perf_counter()
     print(f"{date()}## Test end, test mse is {test_loss:.6f}, time used {end_time - start_time:.0f} seconds.")
 
 
 if __name__ == '__main__':
     config = Config()
+    print(config)
     print(f'{date()}## Load embedding and data...')
-    word2vec = KeyedVectors.load_word2vec_format('data/embedding/GoogleNews-vectors-negative300.bin', binary=True)
-    word_emb = word2vec.vectors
-    word_dict = {w: i.index for w, i in word2vec.vocab.items()}
+    word_emb, word_dict = load_embedding(config.word2vec_file, config.PAD_WORD)
 
-    train_dataset = DeepCoNNDataset('data/music/train.csv', word_dict, config)
-    valid_dataset = DeepCoNNDataset('data/music/valid.csv', word_dict, config, retain_rui=False)
-    test_dataset = DeepCoNNDataset('data/music/test.csv', word_dict, config, retain_rui=False)
+    train_dataset = DeepCoNNDataset(config.train_file, word_dict, config)
+    valid_dataset = DeepCoNNDataset(config.valid_file, word_dict, config, retain_rui=False)
+    test_dataset = DeepCoNNDataset(config.test_file, word_dict, config, retain_rui=False)
     train_dlr = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
-    valid_dlr = DataLoader(valid_dataset, batch_size=config.batch_size, shuffle=True)
-    test_dlr = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=True)
+    valid_dlr = DataLoader(valid_dataset, batch_size=config.batch_size)
+    test_dlr = DataLoader(test_dataset, batch_size=config.batch_size)
 
     model = DeepCoNN(config, word_emb).to(config.device)
-    del train_dataset, valid_dataset, test_dataset, word2vec, word_emb, word_dict
+    del train_dataset, valid_dataset, test_dataset, word_emb, word_dict
 
-    os.makedirs('model', exist_ok=True)  # 文件夹不存在则创建
-    model_Path = 'model/best_model.pt'
-    train(train_dlr, valid_dlr, model, config, model_Path)
-    test(test_dlr, model_Path)
+    os.makedirs(os.path.dirname(config.model_file), exist_ok=True)  # 文件夹不存在则创建
+    train(train_dlr, valid_dlr, model, config, config.model_file)
+    test(test_dlr, torch.load(config.model_file))
